821664e208e1593d664a4cf9a662475735b4f8a9
llondon6 <lionel.london@ligo.org>
2016-04-24 10:36:52 +0100

    adding example for  manual extraction_parameter usage

---
+++ examples/extraction_paramerter_example.py
@@ -0,0 +1,31 @@
+#
+# This example shows how to load data (create gwfylm objects) with the extraction_parameter input. This example assumes the user has installed the Cardiff-UIB dataset.
+#
+# lionel.london@ligo.org
+#
+
+#
+from matplotlib.pyplot import *
+from os import system
+system('clear')
+
+#
+from nrutils.core.nrsc import *
+
+# Search for simulations
+A = scsearch(keyword="base",unique=True,verbose=True)
+
+# Load waveforms using different exraction parameters. NOTE that if the extraction parameter input is not give, then the extraction parameter in the appropriate configuration file will be used. This default value may have also been set independently in the current offcial method for configuring nrutils.
+y = []; lev = [3,4,5]
+for v in lev:
+    y.append( gwylm( scentry_obj = A[0], lm=[2,2], dt=0.4, verbose=True, extraction_parameter=v ) )
+
+# Compare the two waveforms by plotting
+figure(); clr=rgb( len(y) )
+a = [];
+for k in range(len(y)):
+    plot( y[k].ylm[0].t, y[k].ylm[0].amp, color=clr[k], label='lev%i'%lev[k] )
+
+#
+pylim( y[k].ylm[0].t, y[k].ylm[0].amp )
+legend(frameon=False); show()
