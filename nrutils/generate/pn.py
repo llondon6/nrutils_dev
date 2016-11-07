70de6375454ff5244d73afc0350a8a5c17b581ee
llondon6 <lionel.london@ligo.org>
2016-11-07 15:51:06 +0000

    adding some pseudo code

--- nrutils/generate/pn.py
+++ nrutils/generate/pn.py
@@ -1,4 +1,17 @@
 # Module to calculate PN strain waveforms. The output will be members of the gwylm class.
 
-# Import the core 
+# Import the core
 from nrutils.core import *
+
+# A class for the waveform generation workflow
+class genpn:
+
+    #
+    def __init__(this,verbose):
+        return None
+
+# Core generator
+
+# RK4 Solver (put in basics.py)
+
+# Separate modules for RHS? Different PN orders?
